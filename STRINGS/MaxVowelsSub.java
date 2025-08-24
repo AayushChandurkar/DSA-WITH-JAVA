13f6a78fda95228c25435cfb8e5268ede6b33c95
Aayush <aayushchandurkar@gmail.com>
2025-08-24 23:18:24 +0530

    Create MaxVowelsSub.java

---
+++ STRINGS/MaxVowelsSub.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxVowels(String s, int k) {
+        int count = 0;
+        int max = 0;
+        int win = 0;
+        for(int i = 0; i<k; i++){
+            char c = s.charAt(i);
+            if(c == 'a'||c == 'e'||c == 'i'||c == 'o'||c == 'u'){
+                count++;
+            }
+        } 
+        max = count;
+        if(max == k){
+            return max;
+        }
+        else{
+            for(int i = k; i<s.length(); i++){
+                char c = s.charAt(i);
+                char d = s.charAt(i-k);
+                if(c == 'a'||c == 'e'||c == 'i'||c == 'o'||c == 'u'){
+                    count++;
+                }
+                if(d == 'a'||d == 'e'||d == 'i'||d == 'o'||d == 'u'){
+                    count--;
+                }
+                max = Math.max(max,count);
+                if(max == k){
+                    return max;
+                }
+            }
+        }
+        return max;
+    }
+}
